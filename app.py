30560864e2c926d5bccc52cbba4503c8a708e180
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-06-16 15:41:17 -0400

    data file added

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if gemini_api_key:
 else:
     print(f"Gemini key does not exist")
 
-system_message = "You are an assistant and give a cost estimation for the broken windshield of a car in a rude manner."
+system_message = "You are an assistant and give a cost estimation for the broken windshield of a car."
 user_prompt = "what is the estimated cost for the broken windshield of a car?"
 
 prompts = [
